c919b685feba451e6b340cca4e9a44da6271f630
Dalanth <niico.ex@gmail.com>
2013-06-27 12:33:15 -0400

    modificacion nombre clase

--- table.py
+++ table.py
@@ -2,26 +2,26 @@
 
 # Form implementation generated from reading ui file 'table.ui'
 #
-# Created: Thu Jun 27 11:55:21 2013
+# Created: Thu Jun 27 12:32:03 2013
 #      by: pyside-uic 0.2.13 running on PySide 1.1.1
 #
 # WARNING! All changes made in this file will be lost!
 
 from PySide import QtCore, QtGui
 
-class Ui_Form(object):
-    def setupUi(self, Form):
-        Form.setObjectName("Form")
-        Form.resize(500, 460)
-        self.tableView = QtGui.QTableView(Form)
+class Ui_Table(object):
+    def setupUi(self, Table):
+        Table.setObjectName("Table")
+        Table.resize(500, 460)
+        self.tableView = QtGui.QTableView(Table)
         self.tableView.setGeometry(QtCore.QRect(10, 10, 480, 440))
         self.tableView.setMinimumSize(QtCore.QSize(480, 440))
         self.tableView.setMaximumSize(QtCore.QSize(480, 440))
         self.tableView.setObjectName("tableView")
 
-        self.retranslateUi(Form)
-        QtCore.QMetaObject.connectSlotsByName(Form)
+        self.retranslateUi(Table)
+        QtCore.QMetaObject.connectSlotsByName(Table)
 
-    def retranslateUi(self, Form):
-        Form.setWindowTitle(QtGui.QApplication.translate("Form", "Form", None, QtGui.QApplication.UnicodeUTF8))
+    def retranslateUi(self, Table):
+        Table.setWindowTitle(QtGui.QApplication.translate("Table", "Form", None, QtGui.QApplication.UnicodeUTF8))
 
