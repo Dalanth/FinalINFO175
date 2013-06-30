64649119b317f90905feeee2529a729052d7992d
Dalanth <niico.ex@gmail.com>
2013-06-30 01:10:08 -0400

    Botones agregar, editar y eliminar movidos a menubar. Agregados los widgets

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import sys
 import view_form
 from PySide import QtGui, QtCore
 from mainwindow import Ui_MainWindow
+from table import Ui_Table
 
 class MainWindow(QtGui.QMainWindow):
     
@@ -15,18 +16,22 @@ class MainWindow(QtGui.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.set_listeners()
-        self.show()
+        #self.show()
         #durr
+
 
     def delete(self):
     #Calls on controller to delete a product row on the database
         print "estoy eliminando"
 
+
     def set_listeners(self):
     #Sets up button listeners
-        self.ui.btn_delete.clicked.connect(self.delete)
-        self.ui.btn_add.clicked.connect(self.show_add_form)
-        self.ui.btn_edit.clicked.connect(self.show_edit_form)
+        self.ui.btn_delete.triggered.connect(self.delete)
+        self.ui.add.triggered.connect(self.show_add_form)
+        self.ui.edit.triggered.connect(self.show_edit_form)
+        self.ui.quit.triggered.connect(self.close)
+
 
     def show_add_form(self):
     #Displays the add products screen
@@ -39,10 +44,14 @@ class MainWindow(QtGui.QMainWindow):
         form = view_form.Form(self)
         form.exec_()
 
+
+
 def run():
     app = QtGui.QApplication(sys.argv)
     main = MainWindow()
+    main.show()
     sys.exit(app.exec_())
+
 
 if __name__=='__main__':
     run()
