9756efb73270a3a3002a5ddfc8f2ddde3ebcce21
smatamala <simatamala91@gmail.cl>
2013-06-30 23:40:01 -0400

    ventana abrir

--- view_form.py
+++ view_form.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 # ­*­ coding: utf­8 ­*­
-
-from PySide import QtGui, QtCore
-
+from PySide import QtGui, QtCore 
 import controller
 from form import Ui_Form
+
 
 class Form(QtGui.QDialog):
 
@@ -18,6 +17,7 @@ class Form(QtGui.QDialog):
 		else:
 			self.setWindowTitle(u"Editar animal")
 			self.ui.btn_add.clicked.connect(self.edit)
+		self.ui.btn_open.clicked.connect(self.abrir)
 		self.ui.btn_cancel.clicked.connect(self.cancel)
 
 
@@ -28,7 +28,11 @@ class Form(QtGui.QDialog):
 	def edit(self):
 		#Edit an existent animal in the database
 		print "edito animal"
-
+	def abrir(self):
+		#abre ventana para buscar imagen en el directorio
+		NameImage  =  QtGui.QFileDialog . getOpenFileName (self,"Abrir imagen" , "/ home" , "*.png *.jpg *.bmp")
+		print NameImage
+		print "abrir imagen"
 
 	def cancel(self):
 		#Cancel the operation on the ui form
