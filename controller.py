9f2ff561b8491d7685ed6069de4dbcd53a0bc901
Dalanth <niico.ex@gmail.com>
2013-07-03 22:48:26 -0400

    Agregados metodos de busqueda

--- controller.py
+++ controller.py
@@ -13,11 +13,12 @@ def get_animal(nombre_comun):
     #Gets Data from the database for display
     con = connect()
     c = con.cursor()
-    query = "SELECT * FROM animal WHERE nombre_comun = ?"
+    query = """SELECT * FROM animal WHERE nombre_comun = ?"""
     result = c.execute(query, [nombre_comun])
     animal = result.fetchone()
     con.close()
     return animal
+
 
 def get_animals():
     con = connect()
@@ -28,15 +29,93 @@ def get_animals():
     con.close()
     return animals
 
+
+def get_animals_by_type(id_tipo):
+    con = connect()
+    c = con.cursor()
+    query = """SELECT a.id_animal, a.nombre_comun, b.nombre as 'tipos'
+            FROM animal a, tipo b WHERE a.fk_id_tipo = b.id_tipo
+            AND a.fk_id_tipo = ?"""
+    result = c.execute(query, [id_tipo])
+    animals = result.fetchall()
+    con.close()
+    return animals
+
+
+def get_animals_name():
+    con = connect()
+    c = con.cursor()
+    query = """SELECT * FROM animal"""
+    result = c.execute(query)
+    animals = result.fetchall()
+    wordlist = []
+    for row in range(len(animals)):
+        for word in range(1,5):
+            wordlist.append(str(animals[row][word]))
+    types = get_types()
+    con.close()
+    return wordlist
+
+
+def get_types():
+    con = connect()
+    c = con.cursor()
+    query = """SELECT id_tipo, nombre FROM tipo"""
+    result = c.execute(query)
+    types = result.fetchall()
+    con.close()
+    return types
+
+
+def delete(animal):
+    exito = False
+    con = connect()
+    c = con.cursor()
+    query = "DELETE FROM animal WHERE nombre_comun = ?"
+    try:
+        result = c.execute(query, [animal])
+        con.commit()
+        exito = True
+    except sqlite3.Error as e:
+        exito = False
+        print "Error:", e.args[0]
+    con.close()
+    return exito
+
+
+def search_animal(word):
+    con = connect()
+    c = con.cursor()
+    query = """SELECT a.id_animal, a.nombre_comun, a.nombre_cientifico, a.datos, b.nombre as 'tipos'
+            FROM animal a, tipo b WHERE a.fk_id_tipo = b.id_tipo
+            AND (a.nombre_comun LIKE '%'||?||'%' OR a.nombre_cientifico LIKE '%'||?||'%' OR a.datos
+                LIKE '%'||?||'%')"""
+
+    result = c.execute(query, [word, word, word])
+    animals = result.fetchall()
+    con.close()
+    return animals
+
+
 def get_type(nombre):
-    con=connect()
-    c=con.cursor()
-    query="SELECT fk_id_tipo FROM animal WHERE nombre_comun=?"
-    result=c.execute(query,[nombre])
-    tipo=result.fetchone()
-    print(tipo[0])
-    query="SELECT nombre FROM tipo WHERE id_tipo=?"
-    result=c.execute(query,[tipo[0]])
-    tipo=result.fetchone()
-    print(tipo[0])
+    con = connect()
+    c = con.cursor()
+    query = """SELECT fk_id_tipo FROM animal WHERE nombre_comun = ?"""
+    result = c.execute(query,[nombre])
+    tipo = result.fetchone()
+    #print(tipo[0])
+    query = """SELECT nombre FROM tipo WHERE id_tipo = ?"""
+    result = c.execute(query,[tipo[0]])
+    tipo = result.fetchone()
+    #print(tipo[0])
     return tipo
+
+
+def get_types():
+    con = connect()
+    c = con.cursor()
+    query = """SELECT id_tipo, nombre FROM tipo"""
+    result = c.execute(query)
+    types = result.fetchall()
+    con.close()
+    return types
--- main.py
+++ main.py
@@ -16,11 +16,44 @@ class MainWindow(QtGui.QMainWindow):
         self.ui.setupUi(self)
         self.set_listeners()
         self.load_animals()
+        self.load_types()
 
 
     def delete(self):
     #Calls on controller to delete a product row on the database
-        print "estoy eliminando"
+        model = self.ui.tableView.model()
+        index = self.ui.tableView.currentIndex()
+        if index.row() == -1: #No row selected
+            self.ui.errorMessageDialog = QtGui.QMessageBox.information(self, 'Error',
+                                            u"Debe seleccionar el animal que desea eliminar",
+                                            QtGui.QMessageBox.Ok, QtGui.QMessageBox.Ok)
+            return False
+        else:
+            self.ui.confirmMessage = QtGui.QMessageBox.question(self, 'Eliminar animal',
+                                    u"Está seguro que desea eliminar el animal seleccionado?",
+                                    QtGui.QMessageBox.Yes | QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+
+            if self.ui.confirmMessage == QtGui.QMessageBox.Yes:#Prompts the user for confirmation
+                animal = model.index(index.row(), 0, QtCore.QModelIndex()).data()
+                if (controller.delete(animal)):
+                    self.load_animals()
+                    self.ui.msgBox = QtGui.QMessageBox.information(self, u'Atención',
+                                    u"El animal fue eliminado con éxito")
+                    return True
+                else:
+                    self.ui.errorMessageDialog = QtGui.QMessageBox.information(self, 'Error',
+                                            u"Error al eliminar el animal",
+                                            QtGui.QMessageBox.Ok, QtGui.QMessageBox.Ok)
+                    return False
+
+
+    def load_types(self):
+        #Carga los tipos de animales en el combobox
+        types = controller.get_types()
+        self.ui.searchBox.addItem("Todos", -1)
+        for type1 in types: #Agrega los tipos al combobox
+            self.ui.searchBox.addItem(type1["nombre"], type1["id_tipo"])
+        self.ui.searchBox.setEditable(False)
 
 
     def load_animals(self, animals=None):
@@ -39,6 +72,26 @@ class MainWindow(QtGui.QMainWindow):
         self.ui.tableView.setColumnWidth(1, 330)
 
 
+    def load_products_by_search(self):
+        word = self.ui.search.text()
+        animalslist = controller.get_animals_name()
+        animals = controller.search_animal(word)
+        self.load_animals(animals)
+        completer = QtGui.QCompleter(animalslist, self)
+        completer.setCaseSensitivity(QtCore.Qt.CaseInsensitive)
+        completer.setCompletionMode(QtGui.QCompleter.InlineCompletion)
+        self.ui.search.setCompleter(completer)
+
+
+    def load_animals_by_type(self):
+        id_tipo = self.ui.searchBox.itemData(self.ui.searchBox.currentIndex())
+        if id_tipo == -1: #si la opcion seleccionada es "todos", muestra todos los animales
+            animals = controller.get_animals()
+        else: #carga los animales por tipo
+            animals = controller.get_animals_by_type(id_tipo)
+        self.load_animals(animals)
+
+
     def set_listeners(self):
     #Sets up button listeners
         self.ui.btn_delete.triggered.connect(self.delete)
@@ -46,6 +99,8 @@ class MainWindow(QtGui.QMainWindow):
         self.ui.edit.triggered.connect(self.show_edit_form)
         self.ui.quit.triggered.connect(self.close)
         self.ui.tableView.clicked.connect(self.display_data)
+        self.ui.searchBox.activated[int].connect(self.load_animals_by_type)
+        self.ui.search.textChanged.connect(self.load_products_by_search)
 
 
     def show_add_form(self):
@@ -60,17 +115,17 @@ class MainWindow(QtGui.QMainWindow):
         form.exec_()
         self.ui.success.setText("Agregado")
 
+
     def display_data(self):
-        model=self.ui.tableView.model()
-        index=self.ui.tableView.currentIndex()
-        data=model.index(index.row(), 0, QtCore.QModelIndex()).data()
-        animal=controller.get_animal(data)
-        tipo=controller.get_type(data)
+        model = self.ui.tableView.model()
+        index = self.ui.tableView.currentIndex()
+        data = model.index(index.row(), 0, QtCore.QModelIndex()).data()
+        animal = controller.get_animal(data)
+        tipo = controller.get_type(data)
         self.ui.common.setText(animal[1])
         self.ui.cientific.setText(animal[2])
         self.ui.type.setText(tipo[0])
         self.ui.data.setText(animal[3])
-        
 
 
 def run():
