023be81704a7d783a6b858078567b722a98aaa09
Dalanth <niico.ex@gmail.com>
2013-07-04 11:58:18 -0400

    Errores corregidos

--- view_form.py
+++ view_form.py
@@ -7,36 +7,39 @@ from form import Ui_Form
 
 class Form(QtGui.QDialog):
 
-	def __init__(self, parent=None, common_name=None):
-		QtGui.QDialog.__init__(self, parent)
-		self.ui = Ui_Form()
-		self.ui.setupUi(self)
-		if common_name is None:
-			self.ui.btn_done.clicked.connect(self.add)
-		else:
-			self.setWindowTitle(u"Editar animal")
-			self.ui.btn_add.clicked.connect(self.edit)
-		self.ui.btn_cancel.clicked.connect(self.cancel)
+    def __init__(self, parent=None, common_name=None):
+        QtGui.QDialog.__init__(self, parent)
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        types = controller.get_types()
+        for tipo in types:
+            self.ui.typeBox.addItem(tipo["nombre"], tipo["id_tipo"])
+        if common_name is None:
+            self.ui.btn_done.clicked.connect(self.add)
+        else:
+            self.setWindowTitle(u"Editar animal")
+            self.ui.btn_add.clicked.connect(self.edit)
+        self.ui.btn_cancel.clicked.connect(self.cancel)
+        
+
+    def add(self):
+        #Add a new animal
+        common = self.ui.common_name.toPlainText()
+        cientific = self.ui.cientific_name.toPlainText()
+        other = self.ui.data.toPlainText()
+        tipo = self.ui.typeBox.currentText()
+        id_type = controller_form.get_id_type(tipo)
+        result = controller.add_animal(common, cientific, other, id_type)
+        if result:
+            self.reject()
+        else:
+            self.ui.message.setText("Hubo un problema al intentar agregar el animal")
+
+    def edit(self):
+        #Edit an existent animal in the database
+        print "edito animal"
 
 
-	def add(self):
-		#Add a new animal
-		print "agregar animal"
-		Name = self.ui.common_name.toPlainText()
-		CiName = self.ui.cientific_name.toPlainText()
-		datos=self.ui.data.toPlainText()
-		tipo=controller_form.get_id_type(self.ui.typeBox.currentText())
-		result = controller.add_animal(Name, CiName, datos, tipo)
-		if result:
-			self.reject()
-		else:
-			self.ui.message.setText("Hubo un problema al intentar agregar el animal")
-
-	def edit(self):
-		#Edit an existent animal in the database
-		print "edito animal"
-
-
-	def cancel(self):
-		#Cancel the operation on the ui form
-		self.reject()
+    def cancel(self):
+        #Cancel the operation on the ui form
+        self.reject()
